3c8ed094142517a881c0011005a5007ca0c3f314
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-08-22 22:08:55 +0530

    Update callmevp.py

--- callmevp.py
+++ callmevp.py
@@ -1551,138 +1551,8 @@ async def ping(e):
         end = datetime.now()
         ms = (end-start).microseconds / 1000
         await event.edit(f"🤖 𝗣𝗼𝗻𝗴!\n`{ms}`\n 『ᴠɪsᴀ sᴘᴀᴍ ʙᴏᴛ』\n Powered by @MissVisa_Official ")
+        
 
-
-    
-       
-# INVITE ALL OP
-
-from telethon.errors import (
-    ChannelInvalidError,
-    ChannelPrivateError,
-    ChannelPublicGroupNaError,
-)
-from telethon.tl import functions
-from telethon.tl.functions.channels import GetFullChannelRequest
-from telethon.tl.functions.messages import GetFullChatRequest
-
-
-async def get_chatinfo(event):
-    chat = event.pattern_match.group(1)
-    chat_info = None
-    if chat:
-        try:
-            chat = int(chat)
-        except ValueError:
-            pass
-    if not chat:
-        if event.reply_to_msg_id:
-            replied_msg = await event.get_reply_message()
-            if replied_msg.fwd_from and replied_msg.fwd_from.channel_id is not None:
-                chat = replied_msg.fwd_from.channel_id
-        else:
-            chat = event.chat_id
-    try:
-        chat_info = await event.client(GetFullChatRequest(chat))
-    except:
-        try:
-            chat_info = await event.client(GetFullChannelRequest(chat))
-        except ChannelInvalidError:
-            await event.reply("`Invalid channel/group`")
-            return None
-        except ChannelPrivateError:
-            await event.reply(
-                "`This is a private channel/group or I am banned from there`"
-            )
-            return None
-        except ChannelPublicGroupNaError:
-            await event.reply("`Channel or supergroup doesn't exist`")
-            return None
-        except (TypeError, ValueError):
-            await event.reply("`Invalid channel/group`")
-            return None
-    return chat_info
-
-
-def make_mention(user):
-    if user.username:
-        return f"@{user.username}"
-    else:
-        return inline_mention(user)
-
-
-def inline_mention(user):
-    full_name = user_full_name(user) or "No Name"
-    return f"[{full_name}](tg://user?id={user.id})"
-
-
-def user_full_name(user):
-    names = [user.first_name, user.last_name]
-    names = [i for i in list(names) if i]
-    full_name = " ".join(names)
-    return full_name
-
-
-            
-@idk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@ydk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@wdk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@hdk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@sdk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@adk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@bdk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@cdk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@edk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@ddk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall")) 
-@vkk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall")) 
-@kkk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall")) 
-@lkk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall")) 
-@mkk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall")) 
-@sid.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@shy.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@aan.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@ake.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@eel.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@khu.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@shi.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@yaa.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@dav.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@raj.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@put.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-
-async def get_users(event):
-    if event.sender_id in SMEX_USERS:
-        rkp = await event.reply("`processing...`")
-    else:
-        rkp = await event.edit("`processing...`")
-    rk1 = await get_chatinfo(event)
-    chat = await event.get_chat()
-    if event.is_private:
-        return await rkp.edit("`Sorry, Can add users here`")
-    s = 0
-    f = 0
-    error = "None"
-
-    await rkp.edit("**TerminalStatus**\n\n`Collecting Users.......`")
-    async for user in event.client.iter_participants(rk1.full_chat.id):
-        try:
-            if error.startswith("Too"):
-                return await rkp.edit(
-                    f"**Terminal Finished With Error**\n(`May Got Limit Error from telethon Please try agin Later`)\n**Error** : \n`{error}`\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people"
-                )
-            await event.client(
-                functions.channels.InviteToChannelRequest(channel=chat, users=[user.id])
-            )
-            s = s + 1
-            await rkp.edit(
-                f"**Terminal Running...**\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people\n\n**× LastError:** `{error}`"
-            )
-        except Exception as e:
-            error = str(e)
-            f = f + 1
-    return await rkp.edit(
-        f"**Terminal Finished** \n\n• Successfully Invited `{s}` people \n• failed to invite `{f}` people"
-    )        
         
 # _______
 
@@ -1791,7 +1661,7 @@ async def restart(e):
 
 async def help(e):
     if e.sender_id in SMEX_USERS:
-       text = "𝐀𝐕𝐀𝐈𝐋𝐀𝐁𝐋𝐄 𝐂𝐎𝐌𝐌𝐌𝐀𝐍𝐃𝐒 𝐈𝐍 『ᴠɪsᴀ sᴘᴀᴍ ʙᴏᴛ』\n\n𝐔𝐓𝐈𝐋𝐒 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.ping\n.restart\n\n𝐔𝐒𝐄𝐑𝐁𝐎𝐓 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.bio\n.join\n.pjoin\n.leave\n.alive\n𝐒𝐏𝐀𝐌 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.spam\n.delayspam\n.bigspam\n.raid\n.replyraid\n.dreplyraid\n\n\nFor more help regarding usage of plugins type plugins name/n Powered by @MissVisa_Official"
+       text = "𝐀𝐕𝐀𝐈𝐋𝐀𝐁𝐋𝐄 𝐂𝐎𝐌𝐌𝐌𝐀𝐍𝐃𝐒 𝐈𝐍 『ᴠɪsᴀ sᴘᴀᴍ ʙᴏᴛ』\n\n𝐔𝐓𝐈𝐋𝐒 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.ping\n.restart\n\n𝐔𝐒𝐄𝐑𝐁𝐎𝐓 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.bio\n.join\n.pjoin\n.leave\n.inviteall\n.alive\n𝐒𝐏𝐀𝐌 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.spam\n.delayspam\n.bigspam\n.raid\n.replyraid\n.dreplyraid\n\n\nFor more help regarding usage of plugins type plugins name/n Powered by @MissVisa_Official"
        await e.reply(text, parse_mode=None, link_preview=None )
 
         
